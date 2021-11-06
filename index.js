01019a1fe74eb919a3c1be84b94700b77830023a
Uzair Ali <uzairali19b@gmail.com>
2021-11-06 17:08:33 +0500

    update operational callbacks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-
+const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'example';
 
@@ -11,25 +11,57 @@ MongoClient.connect(url, (err, client) => {
 
   const db = client.db(dbname);
   const collection = db.collection('uzair');
-  collection.insertOne(
-    { name: 'Uthappizza', description: 'test' },
-    (err, result) => {
-      assert.equal(err, null);
+  //   collection.insertOne(
+  //     { name: 'Uthappizza', description: 'test' },
+  //     (err, result) => {
+  //       assert.equal(err, null);
 
-      console.log('After Insert:\n');
-      console.log(result.ops);
+  //       console.log('After Insert:\n');
+  //       console.log(result.ops);
 
-      collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
+  //       collection.find({}).toArray((err, docs) => {
+  //         assert.equal(err, null);
 
-        console.log('Found:\n');
-        console.log(docs);
+  //         console.log('Found:\n');
+  //         console.log(docs);
 
-        db.dropCollection('uzair', (err, result) => {
-          assert.equal(err, null);
+  //         db.dropCollection('uzair', (err, result) => {
+  //           assert.equal(err, null);
 
-          client.close();
-        });
+  //           client.close();
+  //         });
+  //       });
+  //     }
+  //   );
+  dboper.insertDocument(
+    db,
+    { name: 'Vadonut', description: 'Test' },
+    'uzair',
+    (result) => {
+      console.log('Insert Document:\n', result.ops);
+
+      dboper.findDocuments(db, 'uzair', (docs) => {
+        console.log('Found Documents:\n', docs);
+
+        dboper.updateDocument(
+          db,
+          { name: 'Vadonut' },
+          { description: 'Updated Test' },
+          'uzair',
+          (result) => {
+            console.log('Updated Document:\n', result.result);
+
+            dboper.findDocuments(db, 'uzair', (docs) => {
+              console.log('Found Updated Documents:\n', docs);
+
+              db.dropCollection('uzair', (result) => {
+                console.log('Dropped Collection: ', result);
+
+                client.close();
+              });
+            });
+          }
+        );
       });
     }
   );
